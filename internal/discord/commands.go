d47af3044962c12b7a5114c3978815636761c17e
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-05-30 11:57:39 -0500

    add plain return in function stubs

--- internal/discord/commands.go
+++ internal/discord/commands.go
@@ -5,41 +5,41 @@ import (
 )
 
 func (d *Discord) Kick(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Mute(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Unmute(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Ban(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Unban(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) RemoveNickname(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) SetNickname(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Slowmode(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) SlowmodeOff(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
 
 func (d *Discord) Purge(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+	return
 }
