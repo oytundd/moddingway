3eb9b2b6d903046249c601dfc05eb546d593361c
KingPieson <crazedsportsfan@gmail.com>
2024-10-05 23:17:39 -0400

    use context manager for cleaner command wrapping

--- src/commands/exile_commands.py
+++ src/commands/exile_commands.py
@@ -2,9 +2,9 @@ import discord
 from discord.ext.commands import Bot
 from settings import get_settings
 from services.exile_service import exile_user, unexile_user
-from util import run_command_with_logging, user_has_role
+from util import user_has_role
 from enums import Role
-
+from .helper import create_logging_embed
 
 settings = get_settings()
 
@@ -22,11 +22,12 @@ def create_exile_commands(bot: Bot) -> None:
     async def unexile(interaction: discord.Interaction, user: discord.Member):
         """Unexile the specified user."""
 
-        await run_command_with_logging(interaction, unexile_user, user)
+        async with create_logging_embed(interaction) as logging_embed:
+            await unexile_user(logging_embed, user)
 
-        await interaction.response.send_message(
-            f"Successfully unexiled {user.mention}", ephemeral=True
-        )
+            await interaction.response.send_message(
+                f"Successfully unexiled {user.mention}", ephemeral=True
+            )
 
     @bot.tree.command()
     @discord.app_commands.check(is_user_moderator)
@@ -43,8 +44,9 @@ def create_exile_commands(bot: Bot) -> None:
     ):
         """Exile the specified user."""
 
-        await run_command_with_logging(interaction, exile_user, user, duration, reason)
+        async with create_logging_embed(interaction) as logging_embed:
+            await exile_user(logging_embed, user, duration, reason)
 
-        await interaction.response.send_message(
-            f"Successfully exiled {user.mention}", ephemeral=True
-        )
+            await interaction.response.send_message(
+                f"Successfully exiled {user.mention}", ephemeral=True
+            )
--- src/util.py
+++ src/util.py
@@ -1,5 +1,4 @@
 import discord
-from collections.abc import Coroutine
 from settings import get_settings
 import enums
 
@@ -21,32 +20,6 @@ def log_info_and_embed(embed: discord.Embed, logger, message: str):
 async def send_dm(member: discord.Member, messageContent: str):
     channel = await member.create_dm()
     await channel.send(content=messageContent)
-
-
-# NB this feels very messy, fragile, and easy to mess up
-# There might be a better way to wrap all this in something cleaner
-async def run_command_with_logging(interaction, command: Coroutine, *args):
-    """
-    Wrap a call to business logic with an automatic creation of a message
-    inside the server's logging channel
-    """
-    embed = discord.Embed()
-    log_channel = interaction.guild.get_channel(settings.logging_channel_id)
-    try:
-        embed.set_author(
-            name=interaction.user.display_name,
-            icon_url=interaction.user.display_avatar.url,
-        )
-        embed.timestamp = interaction.created_at
-        embed.description = f"Used `{interaction.command.name}` command in {interaction.channel.mention}"
-        embed.add_field(name="Action", value=f"/{interaction.command.name}")
-
-        await command(embed, *args)
-
-    except Exception as e:
-        embed.add_field(name="Error", value=e)
-    finally:
-        await log_channel.send(embed=embed)
 
 
 async def add_and_remove_role(
