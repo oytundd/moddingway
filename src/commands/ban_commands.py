a832c66326e838292983cf644072d28fffc5cbab
Brian Tran <brtran@umich.edu>
2024-10-07 01:49:04 -0400

    Add ban command

---
+++ src/commands/ban_commands.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext.commands import Bot
+from settings import get_settings
+from services.ban_service import ban_user
+from util import is_user_moderator
+from .helper import create_logging_embed
+
+settings = get_settings()
+
+
+def create_ban_commands(bot: Bot) -> None:
+    @bot.tree.command()
+    @discord.app_commands.check(is_user_moderator)
+    @discord.app_commands.describe(
+        user="User being banned",
+        reason="Reason for ban",
+    )
+    async def ban(
+        interaction: discord.Interaction,
+        user: discord.Member,
+        reason: str,
+    ):
+        """Ban the specified user."""
+
+        async with create_logging_embed(interaction) as logging_embed:
+            await ban_user(logging_embed, user, reason)
+
+            await interaction.response.send_message(
+                f"Successfully banned {user.mention}", ephemeral=True
+            )
