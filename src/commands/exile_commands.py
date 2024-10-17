f1119d6c021c422ea9917f8bde4c317d9ba112a2
Brian Tran <44913669+brtran4@users.noreply.github.com>
2024-10-17 00:48:31 -0400

    Add roulette command to exile user with random duration (#117)

--- src/commands/exile_commands.py
+++ src/commands/exile_commands.py
@@ -1,12 +1,15 @@
 import discord
+import logging
 from discord.ext.commands import Bot
 from settings import get_settings
 from services.exile_service import exile_user, unexile_user
 from util import is_user_moderator, calculate_time_delta
 from typing import Optional
 from .helper import create_logging_embed
+from random import choice
 
 settings = get_settings()
+logger = logging.getLogger(__name__)
 
 
 def create_exile_commands(bot: Bot) -> None:
@@ -53,4 +56,54 @@ def create_exile_commands(bot: Bot) -> None:
 
             await interaction.response.send_message(
                 error_message or f"Successfully exiled {user.mention}", ephemeral=True
+            )
+
+    @bot.tree.command()
+    @discord.app_commands.checks.cooldown(
+        1, 86400, key=lambda i: (i.guild_id, i.user.id)
+    )
+    async def roulette(interaction: discord.Interaction):
+        """Test your luck, fail and be exiled..."""
+        exile_duration_options = [0, 1, 6, 12, 18, 24]
+        rand_choice = choice(exile_duration_options)
+
+        async with create_logging_embed(interaction) as logging_embed:
+            if rand_choice != 0:
+                reason = "roulette"
+                exile_duration = calculate_time_delta(f"{rand_choice}h")
+                error_message = await exile_user(
+                    logging_embed, interaction.user, exile_duration, reason
+                )
+
+                if error_message:
+                    logger.error(f"An error occurred: {error_message}")
+                    await interaction.response.send_message(
+                        "An error occurred while processing the command.",
+                        ephemeral=True,
+                    )
+                    return
+                else:
+                    await interaction.response.send_message(
+                        f"<@{interaction.user.id}> has tested their luck and has utterly failed! <@{interaction.user.id}> has been sent into exile for {rand_choice} hour(s).",
+                        ephemeral=False,
+                    )
+            else:
+                await interaction.response.send_message(
+                    f"<@{interaction.user.id}> has tested their luck and lives another day...",
+                    ephemeral=False,
+                )
+
+    @bot.tree.error
+    async def on_app_command_error(interaction: discord.Interaction, error):
+        # Check if the error is due to a cooldown
+        if isinstance(error, discord.app_commands.CommandOnCooldown):
+            hours_left = int(error.retry_after / 3600)
+            await interaction.response.send_message(
+                f"This command is on cooldown. Please try again in {hours_left} hour(s).",
+                ephemeral=True,
+            )
+        else:
+            # Handle other errors if necessary
+            await interaction.response.send_message(
+                "An error occurred while processing the command.", ephemeral=True
             )
