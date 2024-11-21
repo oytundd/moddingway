2961b1f1fa583caf422ada89722bd9ae9daa492a
Mera <krasnovian7.pub@gmail.com>
2024-11-21 11:52:54 -0800

    backend server standup, initial models added

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userid = models.IntegerField()
+    discorduserid = models.CharField(max_length=20)
+    discordguildid = models.CharField(max_length=20)
+    ismod = models.BooleanField()
+
+    def __str__(self) -> str:
+        return str(self.userid)
+
+class Exile(models.Model):
+    exileid = models.IntegerField()
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    reason = models.TextField()
+    starttimestamp = models.DateTimeField()
+    endtimestamp = models.DateTimeField()
+    exilestats = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (f"User {self.userid} exiled for reason: \"{self.reason}\"")
+
+class Strike(models.Model):
+    strikeid = models.IntegerField()
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    reason = models.TextField()
+    createtimestamp = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return (f"User {self.userid} given strike for reason: \"{self.reason}\"")
--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User, Exile, Strike
+from .serializer import UserSerializer
+
+@api_view(['GET'])
+def get_user(request):
+    return Response(UserSerializer({'userid': 231, 'discorduserid': "discordidhere", 'discordguildid': "guildidhere", 'ismod': False}).data)
